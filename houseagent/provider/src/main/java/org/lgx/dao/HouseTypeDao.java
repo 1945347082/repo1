146803540cea661fddf7d538af67546f49c2a622
Lianggengxing <1945347082@qq.com>
2020-11-01 13:05:05 +0800

    第一次提交项目

---
+++ houseagent/provider/src/main/java/org/lgx/dao/HouseTypeDao.java
@@ -0,0 +1,14 @@
+package org.lgx.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+/**
+ * (Housetype)表数据库访问层
+ *
+ * @author makejava
+ * @since 2020-08-28 14:51:53
+ */
+@Mapper
+public interface HouseTypeDao {
+
+
+}
