b5fe91b10fb0bd1ef94e9596711491ed8aa14fbd
lianggx34557 <lianggx@hundsun.com>
2021-09-29 22:27:47 +0800

    cherry-pick before

--- houseagent/provider/src/main/java/org/lgx/ProviderApplication.java
+++ houseagent/provider/src/main/java/org/lgx/ProviderApplication.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ProviderApplication {
 
     public static void main (String[] args) {
+
         SpringApplication.run(ProviderApplication.class, args);
     }
 
